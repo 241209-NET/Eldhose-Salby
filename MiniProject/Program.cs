3db440c76b7ef661bab67be063888a139bd2b306
ELDHOSE\eldho <eldhosesalby@gmail.com>
2024-12-11 22:58:47 -0500

    quiz application functionalities added

--- MiniProject/Program.cs
+++ MiniProject/Program.cs
@@ -1,9 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 
 class Program
 {
+    static List<Question> quiz=new List<Question>();
+    static int score=0;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("--------------Welcome to the Quiz Application--------------\n");
+        Console.WriteLine("Menu");
+
+        quiz.Add(new Question("1) Name the process of hiding unnecessary details and showing only what is necessary.", "Abstraction"));
+        quiz.Add(new Question("2) Name the process in which child class inherit parent's class.", "Inheritance"));
+        quiz.Add(new Question("3) Name the process in which concrete classes should depend on abstract definition.", "Dependency inversion"));
+        quiz.Add(new Question("4) Which pattern have only one instance of a class?", "Singleton"));
+        quiz.Add(new Question("5) What conversion is required when converting from a larger data type to a smaller data type, as this may result in data loss?", "Explicit"));
+
+        while(true){
+            ShowMenu();
+            string choice=Console.ReadLine();
+
+            switch(choice){
+                case "1":
+                    AddQuestion();
+                    break;
+                case "2":
+                    StartQuiz();
+                    break;
+                case "3":
+                    Console.WriteLine("\nThank you for using the Quiz Application. See you soon.");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Error, invalid choice. Choose from the option");
+                    break;
+            }
+        }    
+    }
+
+
+static void ShowMenu(){
+        Console.WriteLine("1. Add a question");
+        Console.WriteLine("2. Enter the quiz.");
+        Console.WriteLine("3. Exit the application\n");
+        Console.Write("Enter your choice: ");
+        //Console.WriteLine("Total number of questions: 10\n");
+        //Console.WriteLine("Goodluck!\n");
+    }
+
+static void AddQuestion(){
+    Console.Write("Enter a question: ");
+    string questionText=Console.ReadLine();
+
+    Console.Write("Enter the answer: ");
+    string answerText=Console.ReadLine();
+
+    if(string.IsNullOrEmpty(questionText) || string.IsNullOrEmpty(answerText)){
+        Console.WriteLine("Question or answer cannot be empty.");
+    }
+
+    quiz.Add(new Question(questionText,answerText));
+        Console.WriteLine("Question and answer added successfully.");
+    
+
+}
+
+static void StartQuiz(){
+    score =0;
+    foreach(var question in quiz){
+        Console.WriteLine(question.Text);
+        string userAnswer=Console.ReadLine();
+
+        if(userAnswer.Equals(question.Answer,StringComparison.OrdinalIgnoreCase)){
+            score++;
+            Console.WriteLine("\nCorrect\n");
+        }else{
+            Console.WriteLine($"\nIncorrect, the correcr answer is {question.Answer}\n");
+        }
+    }
+    Console.WriteLine($"\nYour total score is {score}/{quiz.Count}");
+    }
+}
+class Question{
+    public string Text{get; set;}
+    public string Answer{get; set;}
+
+    public Question(string text, string answer){
+        Text=text;
+        Answer=answer;
     }
 }
--- MiniProject/obj/Debug/net8.0/MiniProject.AssemblyInfo.cs
+++ MiniProject/obj/Debug/net8.0/MiniProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de0bc6c9a12310c81e0df9c7e004ab843685ed7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045964feeb2193cd9b5ab196590cbb79a870ed22")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
